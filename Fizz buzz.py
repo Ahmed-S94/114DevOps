e2f81c4939680f41cc73fc434674e59b28337d85
Ahmed-S94 <105853978+Ahmed-S94@users.noreply.github.com>
2022-05-24 00:42:54 +0100

    Add files via upload

---
+++ Fizz buzz.py
@@ -0,0 +1,13 @@
+for fizzbuzz in range(100):
+    if fizzbuzz % 3 == 0 and fizzbuzz % 3 == 0:
+        print("fizzbuzz")
+        continue
+    elif fizzbuzz % 3 == 0:
+        print("fizz")
+        continue
+    elif fizzbuzz % 3 == 0:
+        print("buzz")
+        continue
+    print(fizzbuzz)
+
+# All multples of three will return the word 'Fizzbuzz'
